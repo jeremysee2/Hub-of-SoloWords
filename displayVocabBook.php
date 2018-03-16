8836a5dae0f1ad348783a479de564f5538794206
Solomon Tan <solomonbstoner@yahoo.com.au>
2018-03-17 00:09:43 +0800

    Solved the 'search a word' issue
    I had the problem where the word I am searching for will not show up (ie mysqli_stmt_num_rows is 0) even though I am absolutely positive there should be a match. All I had to do was to move '%' from the prepared sql query to the string $searchword.

--- displayVocabBook.php
+++ displayVocabBook.php
@@ -14,10 +14,7 @@ session_start();
 
 */
 
-?>
 
-
-<?php
 
 require_once 'login_database_config.php';
 
@@ -29,93 +26,82 @@ $user_id = mysqli_fetch_array($query_result, MYSQLI_ASSOC);
 $user_id=$user_id['user_id'];
 
 
-
 $sql = "SELECT word_id, word_name, word_meaning FROM word_storage WHERE user_id=?";
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' and isset($_POST["searchfield"])) {
-  $searchword = $_POST["searchfield"];
-  $sql = "SELECT word_id, word_name, word_meaning FROM word_storage WHERE word_name LIKE '%?%' AND user_id=?";
 
-}
+  $searchword = '%' . $_POST["searchfield"] . '%';
+  $sql = "SELECT word_id, word_name, word_meaning FROM word_storage WHERE word_name LIKE ? AND user_id=?";
+
+} 
 
 $stmt = mysqli_prepare($link, $sql);
 
 
-if(mysqli_stmt_param_count($stmt) == 2){
+if(mysqli_stmt_param_count($stmt) === 2){
   mysqli_stmt_bind_param($stmt, "si", $searchword, $user_id);
 } else {
   mysqli_stmt_bind_param($stmt, "i", $user_id);
 }
 
 
-mysqli_stmt_execute($stmt);
+if(!mysqli_stmt_execute($stmt)){
+  error_log("Error executing query in displayvocabbook.php");
+  goto close_statement;
+}
 
-mysqli_stmt_store_result($stmt);
+if(!mysqli_stmt_store_result($stmt)){
+  error_log("Error storing results in displayvocabbook.php");
+  goto close_statement;
+}
 
 
-mysqli_stmt_bind_result($stmt, $word_id, $word_name, $word_meaning);
 
-if (mysqli_stmt_num_rows === 0){ //An empty set is returned
+if (mysqli_stmt_num_rows($stmt) === 0){ //An empty set is returned
   echo "<br><p>No results found. :'(</p>";
 
 } else { //There are word results to display.
+  mysqli_stmt_bind_result($stmt, $word_id, $word_name, $word_meaning);
   echo <<<_END
-  <br>
-  <div class="col-sm-5" align="center">
-  <table class="table table-bordered" >
-  <thead>
-  <tr>
-  <th scope="col">Word name</th>
-  <th scope="col">Word meaning</th>
-  </tr>
-  </thead>
+  <div class="panel-group col-sm-5" id="accordion">
 _END;
 
   while(mysqli_stmt_fetch($stmt)){
     echo <<<_END
-    <tbody>
-    <tr>
-    <th scope="row"></th>
-    </tr>
-    <td> $word_name </td>
-    <td>
 
-      <button data-toggle='collapse' data-target='#showID$word_id'>Meaning</button>
-      <div id='showID$word_id' class='collapse'> $word_meaning </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse$word_id">
+          $word_name
+          </a>
+        </h4>
+      </div>
+      <div id="collapse$word_id" class="panel-collapse collapse">
+        <div class="panel-body">
+        Meaning: $word_meaning.<br><br>
 
-      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalCenterFor$word_id"> Show meaning in modal </button>
-
-      <div class="modal fade" id="modalCenterFor$word_id" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLongTitle"> $word_name </h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            Meaning of the word: $word_meaning
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          </div>
+        More information of the word will be added...
         </div>
       </div>
-      
-    </td>
+    </div>
 
 _END;
   }
+
+  echo <<<_END
+  </div>
+_END;
   
 }
 
 
 close_statement:
   if(isset($stmt))  //Close statement if set
-    mysqli_close($link);
+      mysqli_stmt_close($stmt);
 
 close_connection:
     if(isset($link))   // Close connection if set
         mysqli_close($link);
 ?>
+
